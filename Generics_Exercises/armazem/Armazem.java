e6cc3cfec9ff575988a6cf6ce0b1cb227458c236
Ever <Ever@host.docker.internal>
2024-12-06 06:50:28 -0300

    exercicios

---
+++ Generics_Exercises/armazem/Armazem.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Armazem<T> implements Armazenavel<T> {
+    private Map<String, T> itens;
+
+    public Armazem() {
+        itens = new HashMap<>();
+    }
+
+    public void adicionarAoInventario(String nome, T item) {
+        itens.put(nome, item);
+    }
+
+    public T obterDoInventario(String nome) {
+        return itens.get(nome);
+    }
+}
---
+++ Generics_Exercises/armazem/Armazenavel.java
@@ -0,0 +1,4 @@
+public interface Armazenavel<T> {
+    void adicionarAoInventario(String nome, T item);
+    T obterDoInventario(String nome);
+}
