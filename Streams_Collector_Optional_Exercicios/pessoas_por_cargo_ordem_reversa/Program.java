41ba168c3b4d3ed95f81c8e30a1190ecd821ca15
Ever Santoro <eversantoro@gmail.com>
2024-12-10 09:20:13 -0300

    v6

--- Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Program.java
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Program.java
@@ -1,8 +1,6 @@
 import java.util.*;
 
 public class Program {
-	
-	/*
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa(1, "John Bannins", "Desenvolvedor", 23, 3200);
         Pessoa pessoa2 = new Pessoa(2, "Derry Collier", "Desenvolvedor", 45, 8600);
@@ -32,18 +30,5 @@ public class Program {
         System.out.println();
         System.out.println(pessoas3);
     }
-    */
-
-
-
-	public static void main(String [] args) {
-		System.out.println("{Product Owner=[[4] Diana Hayes Product Owner 33 R$ 7.400,00, [5] Ellen Costa Product Owner 51 R$ 12.500,00, [3] Mikey Berg Product Owner 51 R$ 11.350,00], Desenvolvedor=[[2] Derry Collier Desenvolvedor 45 R$ 8.600,00, [1] John Bannins Desenvolvedor 23 R$ 3.200,00], Analista QA=[[9] Andrei Melendez Analista QA 42 R$ 5.330,00, [10] Branden Irwin Analista QA 21 R$ 2.800,00, [7] Haseeb Roche Analista QA 21 R$ 3.200,00, [8] Jadene Everett Analista QA 39 R$ 4.860,00, [6] Jane Doe Analista QA 42 R$ 5.200,00]}");
-		System.out.println();
-		System.out.println("{Product Owner=[[4] Diana Hayes Product Owner 33 R$ 7.400,00, [3] Mikey Berg Product Owner 51 R$ 11.350,00], Desenvolvedor=[[2] Derry Collier Desenvolvedor 45 R$ 8.600,00, [1] John Bannins Desenvolvedor 23 R$ 3.200,00], Analista QA=[[9] Andrei Melendez Analista QA 42 R$ 5.330,00, [10] Branden Irwin Analista QA 21 R$ 2.800,00]}");
-		System.out.println();
-		System.out.println("{Product Owner=[[4] Diana Hayes Product Owner 33 R$ 7.400,00, [5] Ellen Costa Product Owner 51 R$ 12.500,00, [3] Mikey Berg Product Owner 51 R$ 11.350,00], Desenvolvedor=[[1] John Bannins Desenvolvedor 23 R$ 3.200,00], Analista QA=[[9] Andrei Melendez Analista QA 42 R$ 5.330,00]}");
-	}
-
-
 }
 
